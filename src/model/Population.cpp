97af8b290941a911d712599b5b983369d5c69727
MJKaminski <M.J.Kaminski@stud.elka.pw.edu.pl>
2015-12-15 00:24:07 +0100

    Interfesj Populacji

--- src/model/Population.cpp
+++ src/model/Population.cpp
@@ -1,7 +1,15 @@
 /**
  * \file   Populacja.cpp
  * \author Adrian Stachlewski
+ * \author Michał Kamiński
  * \date   13-12-2015
  * \brief  Klasa przechowująca informacje o populacji
-#include "Population.h"
 */
+
+#include "Population.h"
+
+Creature Population::getBestCreature (std::vector<std::shared_ptr<Product>> products,
+                                      int populationNumber, int mi=20, int lambda=140){
+    Creature best(products);
+    return best;
+}
--- src/model/Population.h
+++ src/model/Population.h
@@ -1,17 +1,26 @@
 /**
- * \file   Creature.cpp
+ * \file   Population.h
  * \author Adrian Stachlewski
+ * \author Michał Kamiński
  * \date   13-12-2015
  * \brief  Klasa przechowująca informacje o populacji
  */
 
 #ifndef PSZT_POPULATION_H
 #define PSZT_POPULATION_H
-
+#include "Creature.h"
 
 class Population {
 
+public:
+    Creature getBestCreature (std::vector<std::shared_ptr<Product>> products,
+                              int populationNumber, int mi, int lambda);
+
+private:
+    using Creatures = std::vector<std::shared_ptr<Creature>>;
+    using CreatureRef = std::shared_ptr<Creature>;
 };
 
 
 #endif //PSZT_POPULATION_H
+
