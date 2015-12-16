eeac0260b56668967a1bb28220184662d315e4b8
Adrian Stachlewski <adrian.stachlewski@gmail.com>
2015-12-16 19:40:44 +0100

    Done

--- src/model/algorithm.cpp
+++ src/model/algorithm.cpp
@@ -8,6 +8,7 @@
 #include "Creature.h"
 #include "Product.h"
 #include "algorithm.h"
+#include "Population.h"
 
 using Products = std::vector<std::shared_ptr<Product>>; /**< Lista produktow */
 
@@ -17,20 +18,29 @@ using Products = std::vector<std::shared_ptr<Product>>; /**< Lista produktow */
  * \param steps Liczba pokoleń, które zostaną wygenerowane
  */
 
-double fitnessFunction(const Creature& creature)
-{
-
- double sumF, sumFTP;
- for(int i=0; i<creature.getSize(); ++i)
- {
-     auto product = creature.getProduct(i);
-     sumF+= product->getFrequency();
-     sumFTP+= product->getFrequency()*product->getLoadingTime()*++i;
- }
- if(sumFTP!=0)
- return sumF/sumFTP;
+double fitnessFunction(const Creature& creature) {
+    double sumF = 0, sumFTP = 0;
+    for(int i=0; i<creature.getSize(); ++i)
+    {
+        auto product = creature.getProduct(i);
+        sumF += product->getFrequency();
+        sumFTP += product->getFrequency()*product->getLoadingTime()*++i;
+    }
+    if(sumFTP!=0)
+        return sumF/sumFTP;
 }
 
 
-Creature algorithm(Products products, int steps, double mutationProbabity);
+Creature algorithm(Products products, int steps, double mutationProbabity) {
+    int lambda = 10;
+    int mi = 20;
+    Population p(products, lambda);
+    for (int i = 0; i < steps; ++i) {
+        auto t = p.randomSelect(mi);
+        auto o = t.getCrossedPopulation();
+        o.mutatePopulation(mutationProbabity);
+        p = p.getBestFromSum(o, lambda, &fitnessFunction);
+    }
+    return p.getFirstCreature();
+}
 
