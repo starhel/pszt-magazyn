43717541dc229c70cb734a2464f1aefe8272228b
emilia-sokol <emilia.sokol94@gmail.com>
2015-12-16 18:07:43 +0100

    Fitness2

--- src/model/algorithm.cpp
+++ src/model/algorithm.cpp
@@ -7,6 +7,7 @@
 
 #include "Creature.h"
 #include "Product.h"
+#include "algorithm.h"
 
 using Products = std::vector<std::shared_ptr<Product>>; /**< Lista produktow */
 
@@ -16,7 +17,20 @@ using Products = std::vector<std::shared_ptr<Product>>; /**< Lista produktow */
  * \param steps Liczba pokoleń, które zostaną wygenerowane
  */
 
-double fitnessFunction(const Creature& creature);
+double fitnessFunction(const Creature& creature)
+{
+
+ double sumF, sumFTP;
+ for(int i=0; i<creature.getSize(); ++i)
+ {
+     auto product = creature.getProduct(i);
+     sumF+= product->getFrequency();
+     sumFTP+= product->getFrequency()*product->getLoadingTime()*i;
+ }
+ if(sumFTP!=0)
+ return sumF/sumFTP;
+}
+
 
 Creature algorithm(Products products, int steps, double mutationProbabity);
 
